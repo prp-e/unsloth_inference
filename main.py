de36ae02044c5067eb8ad0221cd5b34a18bf4e10
Muhammadreza Haghiri <haghiri75@gmail.com>
2025-08-22 22:23:29 +0330

    working on the main file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import torch
+from datetime import datetime
 from unsloth import FastLanguageModel
 
 model_name = "unsloth/Qwen3-4B-Instruct-2507-bnb-4bit"
@@ -9,3 +10,16 @@ model, tokenizer = FastLanguageModel.from_pretrained(
 )
 
 print(f"Model is loaded on {model.device}")
+
+prompt = "What is the meaning of life, universe and everything?"
+
+messages = [
+    {
+        "role" : "user", 
+        "content" : prompt 
+    }
+]
+
+tokenized_text = tokenizer.apply_chat_template(messages)
+
+print(tokenized_text)
