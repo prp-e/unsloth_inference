39c07c2ffccf5e5c0a43b1143b3c66578ca46fad
Muhammadreza Haghiri <haghiri75@gmail.com>
2025-08-22 21:26:10 +0330

    Let's download and test the code

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import torch
 from unsloth import FastLanguageModel
 
 model_name = "unsloth/Qwen3-4B-Instruct-2507-bnb-4bit"
+
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name= model_name,
+    load_in_4bit = True
+)
